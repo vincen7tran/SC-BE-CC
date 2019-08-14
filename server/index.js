83ab5afa06332a6e55858f3488c6bcd58cb95793
Vinh Tran <vincen7.tran@gmail.com>
2019-08-14 12:23:54 -0700

    added try/catch to get vehicles/:id

--- server/index.js
+++ server/index.js
@@ -11,22 +11,29 @@ app.use(bodyParser.json());
 // SmartCar API
 app.get('/vehicles/:id', async (req, res) => {
   const { id } = req.params;
-  const vehicleInfo = await GM.getVehicleInfoService(id);
-  const { data } = vehicleInfo.data;
-  const { status, reason, vin, color, fourDoorSedan, twoDoorCoupe, driveTrain } = data;
+  try {
+    const vehicleInfo = await GM.getVehicleInfoService(id);
+    const { data } = vehicleInfo.data;
+    const { status, reason, vin, color, fourDoorSedan, twoDoorCoupe, driveTrain } = data;
+    
+    if (status === '404') res.status(404).send(`Status: ${status}, ${reason}`);
   
-  if (status === '404') res.status(404).send(`Status: ${status}, ${reason}`);
+    let doorCount = 'N/A';
 
-  const doorCount = fourDoorSedan.value ? 4 : twoDoorCoupe.value ? 2 : 'N/A';
+    if (fourDoorSedan.value === 'True') doorCount = 4;
+    else if (twoDoorCoupe.value === 'True') doorCount = 2;
   
-  const result = {
-    vin: vin.value,
-    color: color.value,
-    doorCount,
-    driveTrain: driveTrain.value
-  };
-
-  res.status(200).send(result);
+    const result = {
+      vin: vin.value,
+      color: color.value,
+      doorCount,
+      driveTrain: driveTrain.value
+    };
+  
+    res.status(200).send(result);
+  } catch (e) {
+    res.status(500).send('Status: 500, Internal Server Error');
+  }
 });
 
 app.listen(PORT, () => console.log(`Listening on Port: ${PORT}`));
